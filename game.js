42eebe1f18d4d24312eed2c3626139e0fca950f0
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-09-23 12:38:18 +0200

    separate styles and add table

---
+++ game.js
@@ -0,0 +1,9 @@
+const table = document.querySelector('.game-process__table');
+const cellsCount = 100;
+if (table) {
+    for (let i = 0; i < cellsCount; i++) {
+        const button = document.createElement('button');
+        button.classList.add('game-process__cell');
+        table.appendChild(button);
+    }
+}
