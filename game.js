180038ded67dbf0797db5ec3a7998a0228937c47
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-10-21 12:31:52 +0200

    Add AI turn

--- game.js
+++ game.js
@@ -10,8 +10,11 @@ let currentPlayer = null;
 // Create array with 100 empty symbols;
 const game = Array(100).fill('_');
 
+// Get table element
+const table = document.querySelector('.game-process__table');
+
 // Change active player
-const nextTurn = () => {
+const nextTurn = async () => {
     // I use the way with array, because in this way we can add additional playes - ['circle', 'cross', 'ampersand'] - with the same logic 
     const playerIndex = currentPlayer ? playerIndexes.indexOf(currentPlayer.name) : -1;
     if (playerIndex < players.length - 1) {
@@ -23,6 +26,35 @@ const nextTurn = () => {
     if (playerSymbol) {
         playerSymbol.src = `images/${currentPlayer.name}--white.svg`;
         playerSymbol.alt = `${currentPlayer.name} symbol`;
+    }
+    if (currentPlayer.name === 'cross') {
+        if (table) {
+            table.classList.toggle('game-process__table--disabled')
+        }
+        await AITurn();
+        if (table) {
+            table.classList.toggle('game-process__table--disabled')
+        }
+    }
+}
+
+const AITurn = async () => {
+    console.log(game)
+    const response = await fetch('https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({board: game}),
+    });
+    const {position} = await response.json();
+    if (!position) {
+        return;
+    }
+    const index = position.y * 10 + position.x;
+    const btn = document.querySelector(`#btn-${index}`);
+    if (btn) {
+        btn.click();
     }
 }
 
@@ -42,11 +74,11 @@ const renderResult = (result) => {
 };
 
 // Add 100 buttons to the table wrapper
-const table = document.querySelector('.game-process__table');
 if (table) {
     for (let i = 0; i < game.length; i++) {
         const button = document.createElement('button');
         button.classList.add('game-process__cell');
+        button.id = `btn-${i}`;
         table.appendChild(button);
         // Add playes's turn functionality
         button.addEventListener('click', () => {
